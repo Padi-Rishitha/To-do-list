4ef2641de3055a40c86a04688960b827535becdd
padi-rishitha-08 <rishithapadi08@gmail.com>
2022-01-19 11:52:05 +0530

    done

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const bodyParser = require("body-parser");
 
 const app= express();
 let items=["CookFood", "GetVegetables"];
-let workItems=[];
 app.set('view engine', "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
-
 app.get("/",function(req,res){
     let today = new Date();
     let options = {
@@ -22,32 +20,17 @@ app.get("/",function(req,res){
     let day=today.toLocaleDateString("en-US",options);
 
        res.render("list",{
-           listTitle :day,
+           kindOfDay:day,
            newListItems: items
         });
     
 });
 
-
 app.post("/",function(req,res){
-
-    let item= req.body.newItem;
-
-    if(req.body.list === "work"){
-        workItems.push(item);
-        res.redirect("/work");
-    }else{
-
-        items.push(item);
-        res.redirect("/");
-    }
+    let item=req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
-
-app.get("/work",function(req,res){
-    res.render("list",{listTitle:"Work List", newListItems:workItems});
-});
-
-
 
 app.listen(3000,function(){
     console.log("Server Started at 3000");
